6c0c28587f1995f0626e6facc79e5f801345cfdc
ladykakki <greta_calamari@hotmail.it>
2022-03-23 17:33:40 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,21 +1,12 @@
-let userName = prompt('Come ti chiami?')
+let userName = prompt('Inserisci il tuo nome');
 console.log(userName);
 
-let userSurname = prompt("Qual'è il tuo cognome");
-console.log(userSurname);
+let userLastName = prompt('Inserisci il tuo cognome');
+console.log(userLastName);
 
-let userFavoriteColor = prompt("Qual'è il tuo colore preferito");
-console.log(userFavoriteColor);
+let userColor = prompt('Qual è il tuo colore preferito?');
+console.log(userColor);
 
+const number = '21';
 
-
-document.getElementById('password').innerHTML 
-
-
-// a{richiesta nome}-->b[finestra dove scrivere il nome]
-// b-->c{richiesta cognome}
-// c-->e[finestra dove scrivere cognome]
-// e-->f{richiesta colore preferito}
-// f-->g[finestra dove scrivere il colore preferito]
-// g-->h[scrivere in html il mokup]
-// h-->i[fine lavoro]
+document.getElementById('password').innerHTML+= userName + userLastName + userColor + number
