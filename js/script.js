713d3dbba49e12f0f0e5959480e2141f12e8f0a9
ladykakki <greta_calamari@hotmail.it>
2022-03-23 13:28:58 +0100

    richieste password

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let userName = prompt('Come ti chiami?')
+console.log(userName);
+
+let userSurname = prompt("Qual'è il tuo cognome");
+console.log(userSurname);
+
+let userFavoriteColor = prompt("Qual'è il tuo colore preferito");
+console.log(userFavoriteColor);
+
+
+document.getElementById('password') = userName + userSurname + userFavoriteColor;
+
+
